50d5f347912b4706b2e63efbb828670110df860c
Chris Daniels <danielschris96@gmail.com>
2023-05-07 23:13:37 -0700

    worked on handlebars and login/sign up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,46 +3,70 @@ const { Blog, User, Comment } = require('../models');
 const withAuth = require('../utils/auth')
 
 router.post('/login', withAuth, async (req, res) => {
-    try {
+  try {
       const userData = await User.findOne({ where: { email: req.body.email } });
       if (!userData) {
-        req.flash('error', 'Incorrect email or password');
-        res.redirect('/login');
-        return;
+          req.flash('error', 'Incorrect email or password');
+          res.redirect('/login');
+          return;
       }
-  
+
       const validPassword = await userData.checkPassword(req.body.password);
       if (!validPassword) {
-        req.flash('error', 'Incorrect email or password');
-        res.redirect('/login');
-        return;
+          req.flash('error', 'Incorrect email or password');
+          res.redirect('/login');
+          return;
       }
-  
+
       req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
+          req.session.user_id = userData.id;
+          req.session.logged_in = true;
+          res.redirect('/homepage'); 
+      });
+
+  } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+  }
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+router.post('/signup', async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+      req.session.save(() => {
+        req.session.userId = userData.id;
+        req.session.loggedIn = true;
         res.redirect('/');
       });
-  
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
-  });
+});
 
-  // GET all blogs
-router.get('/', withAuth, async (req, res) => {
+router.get('/login', (req, res) => {
+    // render the login page
+    res.render('login');
+});
+
+// GET all blogs
+router.get('/', async (req, res) => {
     try {
       const blogsData = await Blog.findAll();
       const blogs = blogsData.map((blog) => blog.get({ plain: true }));
   
       res.render('homepage', { blogs });
+      console.log(blogs);
     } catch (err) {
       res.status(500).json(err);
     }
-  });
+});
 
-  // GET dashboard
+// GET dashboard
 router.get('/dashboard', withAuth, async (req, res) => {
     try {
       const userData = await User.findByPk(req.session.user_id, {
@@ -57,6 +81,6 @@ router.get('/dashboard', withAuth, async (req, res) => {
     } catch (err) {
       res.status(500).json(err);
     }
-  });
-  
-  module.exports = router;
+});
+
+module.exports = router;
